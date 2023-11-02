d6c4c0cbbff02f0f0e40d767efec508329d38c78
Agim <agimhoti6@gmail.com>
2023-11-02 02:32:19 -0400

    Initial Commit

---
+++ BistroAssist/BistroAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BistroAssistApp.swift
+//  BistroAssist
+//
+//  Created by Agim Hoti on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BistroAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
