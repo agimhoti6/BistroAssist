d1eed659b40ebb90e2f52b0307b9ebbd569bac2a
agimhoti6 <114160359+agimhoti6@users.noreply.github.com>
2023-11-08 03:54:28 -0500

    Add files via upload

---
+++ BistroAssist/MenuView.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+
+struct FoodMenuView: View {
+    var foodItems: [FoodMenuItem]
+    var drinkItems: [FoodMenuItem]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                
+                Section(header: Text("Foods")) {
+                    ForEach(foodItems) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price, specifier: "%.2f")")
+                        }
+                    }
+                }
+
+                Section(header: Text("Drinks")) {
+                    ForEach(drinkItems) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price, specifier: "%.2f")")
+                        }
+                    }
+                }
+
+            
+            }
+            .navigationTitle("Menu")
+        }
+    }
+}
+
+struct FoodMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodMenuView(
+            foodItems: [
+                FoodMenuItem(name: "Pizza", price: 7.99),
+                FoodMenuItem(name: "Burger", price: 5.99),
+                // ... Add other food items here for the preview
+            ],
+            drinkItems: [
+                FoodMenuItem(name: "Water", price: 1.99),
+                FoodMenuItem(name: "Soda", price: 2.99),
+                // ... Add other drink items here for the preview
+            ]
+        )
+    }
+}
+
