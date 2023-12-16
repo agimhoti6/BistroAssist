8a78164cf495598a9b7e5f109cdbf9baea1beaaf
agimhoti6 <114160359+agimhoti6@users.noreply.github.com>
2023-12-16 05:25:11 -0500

    Update BistroAssistApp.swift

--- BistroAssist/BistroAssistApp.swift
+++ BistroAssist/BistroAssistApp.swift
@@ -1,12 +1,23 @@
-
-
 import SwiftUI
 
 @main
 struct BistroAssistApp: App {
+    @StateObject var rootViewModel = RootViewModel()
+    @StateObject var restaurantManager = RestaurantManager()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if rootViewModel.userIsLoggedIn {
+                ViewThree()
+                    .environmentObject(rootViewModel)
+                    .environmentObject(restaurantManager)
+                    .id(rootViewModel.resetTrigger)
+            } else {
+                ContentView()
+                    .environmentObject(rootViewModel)
+                    .environmentObject(restaurantManager) 
+                    .id(rootViewModel.resetTrigger)
+            }
         }
     }
 }
+
