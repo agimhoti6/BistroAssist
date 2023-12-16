b8bf127cf0154a647550984bb6b88b3f201b5358
agimhoti6 <114160359+agimhoti6@users.noreply.github.com>
2023-12-16 05:25:51 -0500

    Update ContentView.swift

--- BistroAssist/ContentView.swift
+++ BistroAssist/ContentView.swift
@@ -1,21 +1,47 @@
-
-
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var rootViewModel: RootViewModel
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Color(.lightGray)
+                
+                VStack {
+                    Text("BistroAssist")
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.center)
+                        .padding(.bottom)
+                    
+                    Image("restaurant")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(15)
+                    
+              
+                    NavigationLink(destination: ViewTwo().environmentObject(rootViewModel)) {
+                        Text("Enter")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .frame(width: 200, height: 60, alignment: .center)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    .padding()
+                }
+                .padding()
+            }
+            .ignoresSafeArea()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(RootViewModel())
     }
 }
+
