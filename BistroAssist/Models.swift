c74060c849debb9f680a6303a2ca83809f502a99
agimhoti6 <114160359+agimhoti6@users.noreply.github.com>
2023-12-16 05:27:20 -0500

    Update Models.swift

--- BistroAssist/Models.swift
+++ BistroAssist/Models.swift
@@ -1,28 +1,16 @@
 import Foundation
 
+// Represents an item on the food menu, including its name and price.
+struct FoodMenuItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let price: Double
+}
+
+// Represents an icon that can be displayed in the UI alongside a menu item.
 struct Icon: Identifiable {
-    let id: UUID
+    let id = UUID()
     let name: String
     let imageName: String
     let foodMenuItem: FoodMenuItem
-    
-    init(name: String, imageName: String, foodMenuItem: FoodMenuItem) {
-        self.id = UUID()
-        self.name = name
-        self.imageName = imageName
-        self.foodMenuItem = foodMenuItem
-    }
 }
-
-struct FoodMenuItem: Identifiable {
-    let id: UUID
-    let name: String
-    let price: Double
-    
-    init(name: String, price: Double) {
-        self.id = UUID()
-        self.name = name
-        self.price = price
-    }
-}
-
